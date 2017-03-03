af8cf48a6a344630d0a31911149d6d71a4802030
mdrrrm <mmmdrm@gmail.com>
2017-03-03 17:35:42 +0300

    ScrollTop-btn fade in / fade out

--- js/script.js
+++ js/script.js
@@ -21,6 +21,10 @@ $("#scroll-top").on('click', function(e){
 
 
 // Call function
+  // $(window).load(function(e) {
+  //   showScrollTopBtn();
+  // })
+  window.onload = showScrollTopBtn;
 
   $(document).on('scroll', function(e) {
   showScrollTopBtn();
@@ -30,7 +34,7 @@ $("#scroll-top").on('click', function(e){
 // Function
 
 function showScrollTopBtn() {
-  if ((document).scrollTop() > 500 ) {
+  if ($(document).scrollTop() > 500) {
     $('#scroll-top').fadeIn(150);
   }
   else {$('#scroll-top').fadeOut(150)};
