4615c041ce6ca751706c2352d212df05a59cd7d6
mdrrrm <mmmdrm@gmail.com>
2017-03-03 12:25:33 +0300

    minor fixes

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@ $(document).ready(function(){
   });
 });
 
+// $(#scrollTop).on('click', function(e){
+//   e.preventDefault();
+//   var = currentPosition = $(document).scrollTop();
+//   var = scrollTime = currentPosition / 3;
+//   $('body').animate({'scrollTop':0},scrollTime);
+// })
+
+//   $(window).on('scroll', function(e) {
+//   showScrollTopBtn();
+// })
+
+
+// function showScrollTopBtn() {
+//   if ((document).scrollTop() > 500 ) {
+//     $('#scrollTop').fadeIn(150);
+//   }
+//   else {$('#scrollTop').fadeOut(150)};
+// }
